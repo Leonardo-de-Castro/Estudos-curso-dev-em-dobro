343919ab37f703737c059793824a5ebd25262348
Leonardo-de-Castro <leobml@outlook.com>
2023-09-06 12:21:28 -0300

    adicionado as pastas 04 e 05 tipos primitivos e operadores logicos

---
+++ modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,16 @@
+//tipo primitivo string
+let nome = "Leonardo"
+//tipo primitivo number
+let numero = 12
+//tipo primitivo boolean
+let usuarioVerificado = false
+
+if(usuarioVerificado) {
+    console.log("usuário verificado")
+
+} else {
+    console.log("usuário não é verificado")
+}
+
+//tipo primitivo Null
+let pessoa = null
---
+++ modulo-javascript-basico/05-operadores-logicos/index.js
@@ -0,0 +1,22 @@
+/*
+ == ----- igual ----- a==b ----- verdadeiro se a for igual a b.
+
+ === ----- idêntico ---- a===b ----- verdadeiro se a for idêntico a b.
+
+ !== ----- não idêntico ----- a !== b ----- verdadeiro se o a não for idêntico a b.
+
+ != ----- diferente ----- a != b ----- verdadeiro se o a for diferente de b
+
+ < ---- menor que ----- a < b ----- verdadeiro quando a for menor que b.
+
+ <= ---- menor ou igual que ----- a <= b ----- verdadeiro quando a for menor ou igual a b.
+
+ > ----- maior que ------- a > b -------- verdadeiro quando a for maior que b.
+
+ >= maior ou igual que ------ a >= b ------ verdadeiro quando a for maior que b.
+*/
+
+const a = 3
+const b = 3
+
+console.log(a < b)
