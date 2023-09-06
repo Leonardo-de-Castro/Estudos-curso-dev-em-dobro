201b539352c09c86a781753bcf21fb6673cfbe6e
Leonardo-de-Castro <leobml@outlook.com>
2023-09-06 11:16:09 -0300

    adicionado arquivos em index.html com pastas do curso dev em dobro

---
+++ modulo-javascript-basico/02-console-comentarios/index.js
@@ -0,0 +1,2 @@
+//Esse código imprime no console do navegador o texto "Olá Mundo!"
+alert("Olá Mundo!")
---
+++ modulo-javascript-basico/03-declarando-variaveis/index.js
@@ -0,0 +1,12 @@
+{
+var nomePessoa = "Leonardo"
+
+const emailPessoa = "leobml@outlook.com"
+
+let telefonePessoa = "519999999"
+
+}
+
+console.log(nomePessoa)
+console.lo(emailPessoa)
+console.log(telefonePessoa)
